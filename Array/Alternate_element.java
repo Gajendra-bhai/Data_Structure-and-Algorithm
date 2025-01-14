69c7aae1da49bf56e46290f0eebae646b828670a
Dhirendra Prajapati <87705336+Gajendra-bhai@users.noreply.github.com>
2025-01-14 14:17:13 +0000

    practice day 1

---
+++ Array/Alternate_element.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Alternate_element {
+
+    public static void main(String[] args) 
+    {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the size of an array");
+  int arr_size = sc.nextInt();
+  int array[] = new int[arr_size];
+  for(int i = 0; i < arr_size;i++)
+  {
+    array[i] = sc.nextInt();
+  }
+  //Print Alternate Element
+
+  for(int i = 0; i < arr_size; i++)
+  {
+    System.out.println(array[i] + 1);
+  }
+        
+    }
+}
